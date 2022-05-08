d8c9a67ef4e8b3765c28d7abcaaac89bd9dd7de1
ricker0915 <35825394+ricker0915@users.noreply.github.com>
2022-05-08 22:27:15 +0800

    Add files via upload

---
+++ 2022/05/圖片/main.js
@@ -0,0 +1,25 @@
+﻿
+
+/*延遲載圖*/
+var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".articleList .lazy", //物件
+    //threshold: 300, //預載量 預設300
+});
+
+
+/*回版頭*/
+$(function(){
+	$("#gotop").click(function(){
+		$("html,body").stop(true,false).animate({scrollTop:0},700); //設定回頁面頂端
+		return false;	
+	});
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 300){ //設定大於300px才顯示浮層
+            $('#gotop').fadeIn("fast");
+        } else {
+            $('#gotop').stop().fadeOut("fast");
+        }
+    });
+
+}); 
+
