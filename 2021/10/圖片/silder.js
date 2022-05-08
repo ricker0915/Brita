e3eea4d794a36822e6f5352bbffa0989d9e9cd39
ricker0915 <35825394+ricker0915@users.noreply.github.com>
2022-05-08 22:25:00 +0800

    Add files via upload

---
+++ 2021/10/圖片/silder.js
@@ -0,0 +1,43 @@
+﻿var i=0;
+var Timer;
+
+$(function(){
+  $(".picImg").eq(0).show().siblings().hide();   //默認第一張圖片顯示，其他的隱藏
+    //自動輪播
+    TimerBanner();
+
+    //點擊左右按鈕
+    $(".silder_left_btn").click(function(){
+        clearInterval(Timer);
+        i--;   //往左
+        if(i==-1){
+            i=1;
+        }
+        showPic();
+        TimerBanner();
+    });
+    $(".silder_right_btn").click(function(){
+        clearInterval(Timer);
+        i++;   //往右
+        if(i==2){
+            i=0;
+        }
+        showPic();
+        TimerBanner();
+    });
+});
+
+//輪播部分
+function TimerBanner(){
+    Timer = setInterval(function(){
+        i++;
+        if(i==2){
+            i=0;
+        }
+        showPic()
+    },2000);
+}
+//顯示圖片
+function showPic(){
+    $(".picImg").eq(i).show().siblings().hide();
+}
